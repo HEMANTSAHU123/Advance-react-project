78d9dbe3e7dbcdf1e71731e779752b167290d88c
HEMANTSAHU123 <hs1282542@gmail.com>
2025-04-06 11:23:55 +0530

    project complete

--- vite-project/src/store/Contextprovider.jsx
+++ vite-project/src/store/Contextprovider.jsx
@@ -5,12 +5,7 @@ const Contextprovider = (props) => {
 const[showing,setShowing]=useState(0);
  const[userList,setUserList]=useState([])
 
- const addNote=(note)=>{
-    const newList={...note,id:Date.now()}
-    setUserList((prevlist)=>[...prevlist,newList])
-    setShowing((prev)=>prev+1);
-    setTotal((prev)=>prev+1)
- }
+ 
  const searchNote=(searchTerm)=>{
     const filteredList = userList.filter(
       (note) =>
@@ -20,14 +15,10 @@ const[showing,setShowing]=useState(0);
   setShowing(filteredList.length)
   return filteredList
    }
- const deleteNote=(id)=>{
-    setUserList((prevList)=>prevList.filter((note)=>note.id!==id))
-    setShowing((prev)=>prev-1);
-    setTotal((prev)=>prev-1);
- }
+ 
   return (
     <notecontext.Provider value={{
-addNote,deleteNote,total,setTotal,showing,setShowing,userList,setUserList,searchNote
+total,setTotal,showing,setShowing,userList,setUserList,searchNote
     }}>
       {props.children}
     </notecontext.Provider>
